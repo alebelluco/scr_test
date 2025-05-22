818bc27888a4ac9ef5a10bef22b48440cd606180
alebelluco <148531503+alebelluco@users.noreply.github.com>
2025-05-22 17:11:32 +0200

    Update prod_deployment.py

--- prod_deployment.py
+++ prod_deployment.py
@@ -1,3 +1,9 @@
+# Modifiche introdotte nella versione v1 del 22/05/2025
+# Aggiunti number input per correggere le ore tagliate dal tool per problemi di timbratura
+# A140 modificato reparto da MVAR a FUST
+# A151 modificato reparto da FINE a INCO
+# Tagliati a 3 parole max i nomi su mansioni per matcharli con le timbrature (chi aveva + di 3 nomi non veniva correttamente)
+# riga 421 QTA prodotta = pezzi adj dopo sdoppiamento (prima calcolava 2 volte le righe sdoppiate a cavallo del turno)
 import streamlit as st
 import pandas as pd
 from PyPDF2 import PdfReader
@@ -17,20 +23,26 @@ with head_sx:
 with head_dx:
     st.image('logo.png')
 
+# COMMENTI
+# riga 242 vengono filtrate le ore solo dei diretti
+
 # CREDENZIALI ====================
 
 
 token = st.secrets['token']
-psw = st.secrets['password']
+
+token = st.secrets['password']
+
 username = st.secrets['username']
+
 repository_name = st.secrets['repository_name']
+
 file_path = st.secrets['file_path']
 
 db_path_stampa = 'db_stampa'
 db_path_fustella = 'db_fustella'
 db_path_incolla = 'db_inco'
 
-# INFO STATICHE============================================================================
 
 if not st.toggle('Nessun nuovo dato, visualizza storico'):
 
@@ -75,9 +87,6 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
 }
 
 
-
-
-
     reparti = {
         'STM':'STAM',
         'FST':'FUST',
@@ -102,8 +111,14 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
             st.stop()
             pass
 
-    db_fogli = pd.read_excel(path, sheet_name='Base_Dati')
+    db_fogli = ut.upload(path, 'Base_Dati')
+    #db_fogli = pd.read_excel(path, sheet_name='Base_Dati')
     db_fogli = db_fogli[db_fogli.RAGG_ATTIVITA =='PRODUZIONE']
+    db_fogli['DES_REPARTO'] = np.where(db_fogli.COD_MACCHINA == 'A140 ', 'FUST', db_fogli['DES_REPARTO'])
+    db_fogli['DES_REPARTO'] = np.where(db_fogli.COD_MACCHINA == 'A151 ', 'INCO', db_fogli['DES_REPARTO'])
+
+    db_fogli = db_fogli[db_fogli.COD_MACCHINA != 'A176 ']
+    db_fogli = db_fogli[db_fogli.COD_MACCHINA != 'A177 ']
     # db_mansioni è salvato su github criptato
     try:
         db_mansioni = pe.retrieve_file_decrypt(username, token, repository_name, file_path,psw)
@@ -114,10 +129,19 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
         st.error('Problema nel caricamento delle mansioni')
         st.stop()
     
+
+    # correzione nomi + lunghi 3 parole
+    for i in range(len(db_mansioni)):
+        test = db_mansioni.cognome_nome.iloc[i]
+        test_split = str.split(test, sep=' ')
+        if len(test_split)>3:
+            new_name = test_split[0]+' '+test_split[1]+' '+test_split[2]
+            db_mansioni.cognome_nome.iloc[i] = new_name
+
+
     path_vel = st.sidebar.file_uploader('Caricare kpi_lean')
     if not path_vel:
         st.stop()
-
     v_tgt = ut.upload(path_vel, 'VI_LEAN_INDIC_TOT_SAS')
     v_tgt = v_tgt[v_tgt.PROGR_COMMESSA.astype(str)!='nan']
 
@@ -169,10 +193,12 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
                     matricola = words[i-5]
                     cognome = words[i-4]
                     nome = words[i-3] +' '+ words[i-2]
+            
                 elif words[i-7]=='000009':
                     matricola = words[i-6]
                     cognome = words[i-5]
                     nome = words[i-4] +' '+ words[i-3]+' '+words[i-2]
+                    
                 else:
                     matricola = words[i-4]
                     cognome = words[i-3]
@@ -199,6 +225,7 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
     db_presenze['ora_']=[dt.datetime.strptime(data_str,'%H.%M').time() for data_str in db_presenze.Orario.astype(str)]
     db_presenze['data_ora'] = [dt.datetime.combine(db_presenze.data_.iloc[i],db_presenze.ora_.iloc[i]) for i in range(len(db_presenze))]
     #db_presenze=db_presenze[db_presenze.Cognome == 'GIORGIONI']
+
 
     matricole_nomi = db_presenze[['Matricola','Cognome','Nome']].drop_duplicates()
     matricole_nomi['fullname'] = matricole_nomi['Cognome'] +' '+ matricole_nomi['Nome']
@@ -244,7 +271,7 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
         st.warning("Attenzione, il file inizia con un'uscita")
         st.write('Corretta la prima riga con E')
 
-    with st.expander('Registro correzioni file orari'):
+    with st.expander('Registro correzioni file orari', icon=':material/zoom_in:'):
         for i in range(1,len(db_presenze_ok)):
             if ((db_presenze_ok.Tipo.iloc[i-1] == 'E') and (db_presenze_ok.Tipo.iloc[i] != 'U')):
                 st.write(f"Corretto il file alla riga {i} | Data {db_presenze_ok.Data.iloc[i]} | Operatore {db_presenze_ok.Cognome.iloc[i]} {db_presenze_ok.Nome.iloc[i]} modificato da E a U")
@@ -266,7 +293,6 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
     db_ore = db_ore.merge(db_mansioni[['cognome_nome','Mansione','classificazione','turnazione']], how='left', left_on='cognome_nome', right_on='cognome_nome')
     db_ore['turnazione'] = db_ore['turnazione'].fillna('turnista')
     db_ore['classificazione'] = db_ore['classificazione'].fillna('diretto')
-
     delta = 60
     delta_spezzati = 240
 
@@ -320,7 +346,11 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
 
     if st.toggle('Visualizza dettaglio presenze per turno'):
         'db_ore'
-        st.write(db_ore)
+        date_min = db_ore.Data.min()
+        db_ore = db_ore[db_ore.Mansione.astype(str) != 'nan']
+        db_ore['temp'] = [mans[:3]for mans in db_ore.Mansione ]
+        st.write(db_ore[db_ore.Data==date_min].sort_values(by=['temp','Turno','classificazione']))
+        db_ore = db_ore.drop(columns='temp')
     
     # aggiustamento ore di presenza
     db_ore['Presenza']=None
@@ -369,6 +399,7 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
     db_fogli['inizio'] = [dt.datetime(db_fogli.anno.iloc[i], db_fogli.mese.iloc[i], db_fogli.giorno.iloc[i], db_fogli.ora.iloc[i], db_fogli.minuto.iloc[i]) for i in range(len(db_fogli))]
     db_fogli['fine'] = [db_fogli.inizio.iloc[i] + timedelta(hours=db_fogli.DURATA_STEP.iloc[i]) for i in range(len(db_fogli))]
 
+    db_fogli['Data'] = [data.date() for data in db_fogli.DATA_ATTIVITA]
     db_fogli = ut.sdoppia(db_fogli, orari_turni2,'inizio','fine',['COD_MACCHINA','inizio'] )
     db_fogli['durata_calcolata'] = [np.round((db_fogli['fine'].iloc[i] - db_fogli['inizio'].iloc[i]).seconds/3600,5) for i in range(len(db_fogli))]
     db_fogli['appoggio'] = np.where(db_fogli['DURATA_STEP'] != 0, db_fogli['DURATA_STEP'], 1)
@@ -378,18 +409,14 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
     db_fogli['scarti_adj'] = db_fogli['QTA_SCARTI'] * db_fogli['ripartizione']
     db_fogli['key'] = (db_fogli['COD_MACCHINA'] + db_fogli['COD_COMMESSA']).str.replace(" ","")
     db_fogli = db_fogli.merge(v_tgt.groupby('key').max(), how='left', left_on='key', right_on='key') #facendo groupby-max ho preso il valore + alto per ogni key
-    #db_fogli = db_fogli[db_fogli.VEL_TIRATURA_PREVISTA.astype(str) != 'nan']
-    #db_fogli = db_fogli[db_fogli.VEL_TIRATURA_PREVISTA != 0]
     db_fogli['VEL_TIRATURA_PREVISTA'] = db_fogli['VEL_TIRATURA_PREVISTA'].fillna(999999999) #COSì IL RAPPORTO DIVENTA 0
     db_fogli['ton'] = np.where(db_fogli.VEL_TIRATURA_PREVISTA != 0,(db_fogli.pezzi_adj + db_fogli.scarti_adj)*(1/db_fogli.VEL_TIRATURA_PREVISTA),0)
     db_fogli['vel_puntuale'] = db_fogli.ton / (db_fogli.durata_calcolata + 0.0001)
-
-
-
     db_fogli['Data'] = [data.date() for data in db_fogli.DATA_ATTIVITA]
     db_fogli['orario'] = [data[-5:] for data in db_fogli.DATA_ORA.astype(str)]
     db_fogli['ora_']=[dt.datetime.strptime(data_str,'%H.%M').time() for data_str in db_fogli.orario.astype(str)]
     db_fogli['DATA_ATTIVITA'] = [dt.datetime.combine(db_fogli.Data.iloc[i],db_fogli.ora_.iloc[i]) for i in range(len(db_fogli))]
+    db_fogli['QTA_PRODOTTA']=db_fogli['pezzi_adj']
 
     db_fogli['turno']=None
 
@@ -463,18 +490,7 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
 
     #STAMPA
 
-    db_fogli_stam = db_fogli[db_fogli.DES_REPARTO == 'STAM'][['turno','QTA_PRODOTTA','ton']].groupby(by='turno', as_index=False).sum()
-    db_ore_stam = ore_group[ore_group.reparto == 'STM'].merge(db_fogli_stam, how='left',left_on='Turno',right_on='turno').drop(columns='turno')
-    db_ore_stam['F/mh'] = np.round(db_ore_stam.QTA_PRODOTTA.astype(float) / db_ore_stam.Presenza.astype(float),0)
-    db_ore_stam['mh/h_std'] = np.round( db_ore_stam.Presenza.astype(float)/db_ore_stam.ton.astype(float),0)
-
     # FUSTELLA
-
-    db_fogli_fus = db_fogli[db_fogli.DES_REPARTO == 'FUST']
-    db_fogli_fus = db_fogli[db_fogli.DES_REPARTO == 'FUST'][['turno','QTA_PRODOTTA','ton']].groupby(by='turno', as_index=False).sum()
-    db_ore_fus = ore_group[ore_group.reparto == 'FST'].merge(db_fogli_fus, how='left',left_on='Turno',right_on='turno').drop(columns='turno')
-    db_ore_fus['F/mh'] = np.round(db_ore_fus.QTA_PRODOTTA.astype(float) / db_ore_fus.Presenza.astype(float),0)
-    db_ore_fus['mh/h_std'] = np.round( db_ore_fus.Presenza.astype(float)/db_ore_fus.ton.astype(float),0)
 
     # PIEGA INCOLLA
 
@@ -488,12 +504,33 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
 
     with tab1:
         st.subheader(f'Produttività {data_prima}', divider='grey')
-
-        st.subheader('Stampa', divider='grey')
+        st.subheader('Stampa', divider='blue')
+        
         c1_st, c2_st, c3_st, c4_st = st.columns([3,1,1,1])
         with c1_st:
+            with st.expander('Correzione ore turno stampa',icon=":material/edit:"):
+                add_stampa = []
+                add_t1_s = st.number_input('Ore aggiuntive turno 1', step=0.1)
+                add_t2_s = st.number_input('Ore aggiuntive turno 2', step=0.1)
+                add_t3_s = st.number_input('Ore aggiuntive turno 3', step=0.1)
+                add_stampa.append(add_t1_s)
+                add_stampa.append(add_t2_s)
+                add_stampa.append(add_t3_s)       
+
+            db_fogli_stam = db_fogli[db_fogli.DES_REPARTO == 'STAM'][['turno','QTA_PRODOTTA','ton']].groupby(by='turno', as_index=False).sum()
+            db_ore_stam = ore_group[ore_group.reparto == 'STM'].merge(db_fogli_stam, how='left',left_on='Turno',right_on='turno').drop(columns='turno')
+            # correzione con ore aggiunte
+            db_ore_stam=db_ore_stam.rename(columns={'Presenza':'Presenza_draft', 'Ore_totali':'Ore_tot_draft'})
+            db_ore_stam['Presenza'] = db_ore_stam['Presenza_draft'] + add_stampa
+            db_ore_stam['Ore_totali'] = db_ore_stam['Ore_tot_draft'] + add_stampa
+
+            db_ore_stam['F/mh'] = np.round(db_ore_stam.QTA_PRODOTTA.astype(float) / db_ore_stam.Presenza.astype(float),0)
+            db_ore_stam['mh/h_std'] = np.round( db_ore_stam.Presenza.astype(float)/db_ore_stam.ton.astype(float),0)
+
             db_ore_stam
-            st.metric('FOGLI ORA UOMO GIORNATA | indiretti INCLUSI', value='{:0.0f}'.format(db_ore_stam['QTA_PRODOTTA'].sum()/db_ore_stam['Ore_totali'].sum()), border=True)
+            st.divider()
+            st.metric('FOGLI ORA UOMO GIORNATA | indiretti INCLUSI', value='{:0.0f}'.format(db_ore_stam['QTA_PRODOTTA'].sum()/db_ore_stam['Ore_totali'].sum()))
+            st.divider()
         with c2_st:
             st.metric('FOGLI ORA UOMO T1', value=db_ore_stam['F/mh'].iloc[0], border=True)
         with c3_st:
@@ -502,12 +539,33 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
             st.metric('FOGLI ORA UOMO T3', value=db_ore_stam['F/mh'].iloc[2], border=True)
 
 
-        st.subheader('Fustella', divider='grey')
+        st.subheader('Fustella', divider='blue')
         c1_fs, c2_fs, c3_fs, c4_fs = st.columns([3,1,1,1])
         with c1_fs:
-            db_ore_fus
-            st.metric('FOGLI ORA UOMO GIORNATA | indiretti INCLUSI', value='{:0.0f}'.format(db_ore_fus['QTA_PRODOTTA'].sum()/db_ore_fus['Ore_totali'].sum()), border=True)
+            with st.expander('Correzione ore turno fustella',icon=":material/edit:"):
+                add_fustella = []
+                add_t1_f = st.number_input('Ore aggiuntive turno 1', step=0.1, key='f1')
+                add_t2_f = st.number_input('Ore aggiuntive turno 2', step=0.1, key='f2')
+                add_t3_f = st.number_input('Ore aggiuntive turno 3', step=0.1, key='f3')
+                add_fustella.append(add_t1_f)
+                add_fustella.append(add_t2_f)
+                add_fustella.append(add_t3_f) 
 
+            db_fogli_fus = db_fogli[db_fogli.DES_REPARTO == 'FUST']
+            db_fogli_fus = db_fogli[db_fogli.DES_REPARTO == 'FUST'][['turno','QTA_PRODOTTA','ton']].groupby(by='turno', as_index=False).sum()
+            db_ore_fus = ore_group[ore_group.reparto == 'FST'].merge(db_fogli_fus, how='left',left_on='Turno',right_on='turno').drop(columns='turno')
+            # correzione con ore aggiunte
+            db_ore_fus=db_ore_fus.rename(columns={'Presenza':'Presenza_draft', 'Ore_totali':'Ore_tot_draft'})
+            db_ore_fus['Presenza'] = db_ore_fus['Presenza_draft'] + add_fustella
+            db_ore_fus['Ore_totali'] = db_ore_fus['Ore_tot_draft'] + add_fustella
+
+            db_ore_fus['F/mh'] = np.round(db_ore_fus.QTA_PRODOTTA.astype(float) / db_ore_fus.Presenza.astype(float),0)
+            db_ore_fus['mh/h_std'] = np.round( db_ore_fus.Presenza.astype(float)/db_ore_fus.ton.astype(float),0)
+
+            db_ore_fus
+            st.divider()
+            st.metric('FOGLI ORA UOMO GIORNATA | indiretti INCLUSI', value='{:0.0f}'.format(db_ore_fus['QTA_PRODOTTA'].sum()/db_ore_fus['Ore_totali'].sum()))
+            st.divider()
         with c2_fs:
             st.metric('FOGLI ORA UOMO T1', value=db_ore_fus['F/mh'].iloc[0], border=True)
         with c3_fs:
@@ -516,18 +574,36 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
             st.metric('FOGLI ORA UOMO T3', value=db_ore_fus['F/mh'].iloc[2], border=True)
 
 
-        st.subheader('Piega incolla', divider='grey')
+        st.subheader('Piega incolla', divider='blue')
         c1_in, c2_in, c3_in, c4_in = st.columns([3,1,1,1])
         with c1_in:
+            with st.expander('Correzione ore turno piega-incolla',icon=":material/edit:"):
+                add_incolla = []
+                add_t1_i = st.number_input('Ore aggiuntive turno 1', step=0.1, key='i1')
+                add_t2_i = st.number_input('Ore aggiuntive turno 2', step=0.1, key='i2')
+                add_incolla.append(add_t1_i)
+                add_incolla.append(add_t2_i)
+
+            db_fogli_inco = db_fogli[db_fogli.DES_REPARTO == 'INCO'][['turno','QTA_PRODOTTA','ton']].groupby(by='turno', as_index=False).sum()
+            db_ore_inco = ore_group[ore_group.reparto == 'INC'].merge(db_fogli_inco, how='left',left_on='Turno',right_on='turno').drop(columns='turno')
+            db_ore_inco = db_ore_inco[:2]
+            # correzione con ore aggiunte
+            db_ore_inco=db_ore_inco.rename(columns={'Presenza':'Presenza_draft', 'Ore_totali':'Ore_tot_draft'})
+            db_ore_inco['Presenza'] = db_ore_inco['Presenza_draft'] + add_incolla
+            db_ore_inco['Ore_totali'] = db_ore_inco['Ore_tot_draft'] + add_incolla
+
+            db_ore_inco['F/mh'] = np.round(db_ore_inco.QTA_PRODOTTA.astype(float) / db_ore_inco.Presenza.astype(float),0)
+            db_ore_inco['mh/h_std'] = np.round(db_ore_inco.Presenza.astype(float)/db_ore_inco.ton.astype(float),0)
             db_ore_inco
-            st.metric('ORE_uomo / ORE_std GIORNATA | indiretti INCLUSI', value='{:0.2f}'.format(db_ore_fus['Ore_totali'].sum()/db_ore_fus['ton'].sum()), border=True)
+            st.divider()
+            st.metric('ORE_uomo / ORE_std GIORNATA | indiretti INCLUSI', value='{:0.2f}'.format(db_ore_inco['Ore_totali'].sum()/db_ore_inco['ton'].sum()))
 
         with c2_in:
             #st.metric('PEZZI ORA UOMO T1', value=db_ore_inco['F/mh'].iloc[0], border=True)
-            st.metric('ORE_uomo / ORE_std T1', value='{:0.2f}'.format(db_ore_fus['Presenza'].iloc[0]/db_ore_fus['ton'].iloc[0]), border=True)
+            st.metric('ORE_uomo / ORE_std T1', value='{:0.2f}'.format(db_ore_inco['Presenza'].iloc[0]/db_ore_inco['ton'].iloc[0]), border=True)
         with c3_in:
             #st.metric('PEZZI ORA UOMO T2', value=db_ore_inco['F/mh'].iloc[1], border=True)
-            st.metric('ORE_uomo / ORE_std T2', value='{:0.2f}'.format(db_ore_fus['Presenza'].iloc[1]/db_ore_fus['ton'].iloc[1]), border=True)
+            st.metric('ORE_uomo / ORE_std T2', value='{:0.2f}'.format(db_ore_inco['Presenza'].iloc[1]/db_ore_inco['ton'].iloc[1]), border=True)
 
 
         # PRIMO CARICAMENTO SU GITHUB =====================================================
@@ -539,8 +615,8 @@ if not st.toggle('Nessun nuovo dato, visualizza storico'):
         updated_fust = pe.retrieve_file(username, token, repository_name, db_path_fustella)
         updated_inco = pe.retrieve_file(username, token, repository_name, db_path_incolla)
 
-
-        if st.button('Aggiorna database'):
+        st.divider()
+        if st.button('Aggiorna database', icon=':material/upload:'):
 
             try:
                 updated_stam = pd.concat([updated_stam, db_ore_stam]).drop_duplicates().sort_values(by=['Data','Turno'])
@@ -637,7 +713,6 @@ else:
     st.subheader('Andamento Piega-incolla')
     st.plotly_chart(graph_in, key='incolla')
             
-
 
 
 # eliminare indiretti nel confronto turni, ma tenerli in un kpi non diviso per turno
